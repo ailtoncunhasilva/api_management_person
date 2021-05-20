862443d5c5294ade19e6cc9a74d4f178fd401c7e
ailtoncunhasilva <ailtoncunhasilva@hotmail.com>
2021-05-20 17:05:50 -0300

    second commit

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.*;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
